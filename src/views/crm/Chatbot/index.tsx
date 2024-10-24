31e1307d63fbfd10e50dae6021d5134bcb23f35b
janisar007 <134937141+janisar007@users.noreply.github.com>
2024-10-24 16:25:17 +0530

    Update index.tsx

--- src/views/crm/Chatbot/index.tsx
+++ src/views/crm/Chatbot/index.tsx
@@ -1,43 +1,56 @@
 import { Button } from "@/components/ui/Button";
-import { useEffect, useRef, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { LuSendHorizonal } from "react-icons/lu";
 import { IoCopyOutline } from "react-icons/io5";
 import { IoCheckmarkDone } from "react-icons/io5";
-import { Skeleton } from "@/components/ui";
+import { InputGroup, Skeleton } from "@/components/ui";
+import { UserDetailsContext } from '@/views/Context/userdetailsContext'
 import Input from '@/components/ui/Input'
- 
+
 interface Message {
     text: string;
     sender: "user" | "bot";
 }
- 
+
 const Index = () => {
     const [inputValue, setInputValue] = useState('');
     const [messages, setMessages] = useState<any>([]);
- 
-    console.log(messages)
-    // const [data, setData] = useState<any>([]);
+
+    const org_id = localStorage.getItem("orgId")
+    const user_id = localStorage.getItem("userId")
+
     const [copiedMessageIndex, setCopiedMessageIndex] = useState<number | null>(null);
     const [loading, setLoading] = useState<any>(false);
- 
- 
+
+
     const messageRefs = useRef<any>([]);
- 
+    const data = useContext<any>(UserDetailsContext)
+
+    useEffect(() => {
+        const greetingMessage: Message = {
+            text: `Hello ${data && data?.username ? data?.username : "there"}! How can I assist you today?`,
+            sender: 'bot',
+        }
+        setMessages((prevMessages: any) => [...prevMessages, greetingMessage])
+    }, [])
+
     const copyToClipboard = (index: any) => {
-        const textToCopy = messageRefs.current[index].innerText;
+        let textToCopy = messageRefs.current[index].innerText;
+        const regex = /\bCopy\b/;
+        textToCopy = textToCopy.replace(regex, '').trim();
+
         navigator.clipboard.writeText(textToCopy).then(() => {
             console.log('Text copied to clipboard!');
-            setCopiedMessageIndex(index); // Set the copied message index
-        setTimeout(() => setCopiedMessageIndex(null), 2000);
+            setCopiedMessageIndex(index);
+            setTimeout(() => setCopiedMessageIndex(null), 2000);
         }).catch(err => {
             console.error('Failed to copy text: ', err);
         });
-       
- 
     };
- 
-    console.log(messages)
- 
+
+
+
+
     const fetchData = async (inputValue: string) => {
         try {
             setLoading(true);
@@ -46,24 +59,41 @@ const Index = () => {
                 headers: {
                     "Content-Type": "application/json",
                 },
-                body: JSON.stringify({ question: inputValue }),
+                body: JSON.stringify({ question: inputValue, org_id, user_id }),
             });
- 
+
             const reader = response.body?.getReader();
             const decoder = new TextDecoder();
             let accumulatedMessages = "";
- 
+
             if (reader) {
                 while (true) {
                     const { done, value } = await reader.read();
                     if (done) break;
- 
+
                     accumulatedMessages += decoder.decode(value, { stream: true });
                 }
             }
- 
-            console.log(accumulatedMessages)
- 
+
+
+            if (accumulatedMessages.includes("404: Project not found.")) {
+                setMessages((prevMessages: any) => [
+                    ...prevMessages,
+                    { text: "There is no project with this name", sender: "bot" },
+                ]);
+
+                return
+            }
+            if (accumulatedMessages.includes("404: Lead not found.")) {
+
+                setMessages((prevMessages: any) => [
+                    ...prevMessages,
+                    { text: "There is no Lead with this name", sender: "bot" },
+                ]);
+
+                return
+            }
+
             // After receiving all data, we create a single message for the bot
             if (accumulatedMessages.startsWith("data: ")) {
                 const message = accumulatedMessages.slice(6).trim();
@@ -72,7 +102,7 @@ const Index = () => {
                     { text: message, sender: "bot" },
                 ]);
             }
- 
+
         } catch (error) {
             console.error("Error fetching chatbot response:", error);
             setMessages((prevMessages: any) => [
@@ -83,93 +113,92 @@ const Index = () => {
             setLoading(false);
         }
     };
- 
- 
+
+
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setInputValue(event.target.value);
     };
- 
+
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         if (!inputValue.trim()) return;
- 
-        // Add user message to the messages array
+
         setMessages((prevMessages: any) => [
             ...prevMessages,
             { text: inputValue, sender: "user" },
         ]);
- 
-        // Call the chatbot API
+
         fetchData(inputValue);
-        setInputValue(''); // Clear the input field
+        setInputValue('');
     };
- 
+
     return (
-        <div className="h-full">
-            <h2>Chatbot</h2>
-            <form onSubmit={handleSubmit} className="flex flex-col h-full">
-                <div className=" messages flex-1 h-96 overflow-y-auto mb-4 border border-gray-300 rounded-lg p-2">
+        <div className="h-full mb-4">
+            <h2 className="mb-2">Chat Bot</h2>
+            <form onSubmit={handleSubmit} className="flex flex-col h-full ">
+                <div className="bg-gray-100 dark:bg-[#1F2937] messages flex-1 h-96 overflow-y-auto mb-4 border border-gray-300 rounded-lg p-2">
                     {messages.map((message: any, index: any) => (
- 
+
                         <div className={`flex w-full ${message.sender === "user" ? "justify-end" : "justify-start"}`}>
- 
+
                             <div
                                 key={index}
                                 ref={(el) => (messageRefs.current[index] = el)}
-                                className={`relative w-[70%] flex gap-2 flex-col message p-2 rounded mb-2 ${message.sender === "user" ? "bg-gray-200" : "bg-blue-100 pt-6 px-3"
-                                    }`}
- 
+                                className={`relative flex gap-2 flex-col message p-2 rounded mb-2 ${message.sender === "user" ? "bg-blue-100" : "bg-white dark:bg-[#111827] dark:border-none  px-3 border-[0.13rem] border-blue-100 w-[70%]"
+                                    } group`}
+
                             >
- 
- 
+
+
                                 {message.sender === "bot" &&
- 
-                                    <div className="absolute right-0 top-0 flex justify-center items-center m-2 cursor-pointer text-black gap-1" onClick={() => copyToClipboard(index)}>
- 
+
+                                    <div className="bg-gray-100 dark:bg-[#1F2937] dark:text-white p-1 rounded-md absolute right-1 top-1 flex justify-center items-center cursor-pointer text-black gap-1 opacity-0 group-hover:opacity-100" onClick={() => copyToClipboard(index)}>
+
                                         {
-                                            copiedMessageIndex === index ? <IoCheckmarkDone/> : <IoCopyOutline/>
+                                            copiedMessageIndex === index ? <IoCheckmarkDone /> : <IoCopyOutline />
                                         }
-                                       
+
                                         <div>{copiedMessageIndex === index ? "Copied" : `Copy`}</div>
                                     </div>}
- 
- 
- 
-                                {/* <strong>{message.sender === "user" ? "User:" : "Bot:"}</strong> */}
+
+
+
                                 {" "}
                                 {message.sender === "user" ? message.text.split('data: ').map((line: any, lineIndex: any) => (
-                                    <div key={lineIndex}>{line}{lineIndex < message.text.split('\n').length - 1 && <br />}</div>
+                                    <div key={lineIndex} className="flex justify-end"><span>{line}{lineIndex < message.text.split('\n').length - 1 && <br />}</span></div>
                                 )) : message.text.split('data: ').map((line: any, lineIndex: any) => (
-                                    <div key={lineIndex}>{lineIndex + 1}{". "}{line}{lineIndex < message.text.split('\n').length - 1 && <br />}</div>
- 
+                                    <div className="flex justify-start" key={lineIndex}><span>{lineIndex + 1}{". "}{line}{lineIndex < message.text.split('\n').length - 1 && <br />}</span></div>
+
                                 ))}
- 
- 
- 
+
+
+
                             </div>
                         </div>
- 
-))}
-{loading && <Skeleton width={850} height={100}/>}
+
+                    ))}
+                    {loading && <Skeleton width={850} height={100} />}
                 </div>
-                <div className="flex ">
+
+                <InputGroup className="bottom-0">
                     <Input
+                        placeholder="Ask anything..."
                         type="text"
                         value={inputValue}
                         onChange={handleChange}
-                        placeholder="Ask anthing..."
-                        className="flex-1 w-6 text-[1.1rem] p-2 rounded-l-md focus:outline-none bg-[#F4F4F4]"
+                        className="flex-1 w-6 text-[1.1rem] p-2 rounded-l-md"
                         autoFocus
                     />
-                    <Button 
-                    // className="flex justify-start items-center bg-[#F4F4F4] text-black px-2 py-3 rounded-r-md hover:bg-gray-400 transition border-none w-[3rem]" 
-                    type="submit">
-                        <LuSendHorizonal className="text-[1.7rem]" />
-                    </Button>
-                </div>
+                    <Button
+                        icon={<LuSendHorizonal className="text-[1.7rem]" />}
+                        type='submit'
+
+                        className="w-14"
+                    />
+                </InputGroup>
             </form>
         </div>
     );
 };
- 
+
 export default Index;
